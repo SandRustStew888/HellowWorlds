d26a2c9ec62a56c01bcc143d8c07c2b6baa4716f
SandRustStew888 <seth.andrus@sinclair.edu>
2023-10-19 15:42:23 -0400

    initial file

---
+++ HalooWorld.cpp
@@ -0,0 +1,21 @@
+/* * * * * * * * * * * * * * * * * *
+* Seth Andrus
+* CIS 1202 201
+* 2023/Oct/19
+* Hello Git
+* * * * * * * * * * * * * * * * * */
+
+#include<iostream>
+
+
+using namespace std;
+
+
+/* * * * * * * * * * * * * * * * * *
+* Prints to Consoul
+* * * * * * * * * * * * * * * * * */
+int main() {
+
+
+    return 0;
+}
